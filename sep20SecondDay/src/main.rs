f862d717364c5aa203ec6eb5847f5ea51e66c352
Tarak Nath Jana <janatarak739@gmail.com>
2025-09-20 19:28:13 +0530

    save data

--- sep20SecondDay/src/main.rs
+++ sep20SecondDay/src/main.rs
@@ -19,16 +19,41 @@ fn main() {
 
     // first time call void function 
      _first_void_function("hello mather cho".to_string());
+
+
+
+
+
+    //  chack ownership
+    let _owner_ship :String = "this is your data".to_string();
+    let _return_owner_data =  _chack_owner_ship(_owner_ship);
+    println!("{}",_return_owner_data);
+
+
+
+
+    // chack referencing 
+
+    let mut referencing_data = 124; 
+
+    let return_data = _reference(& mut referencing_data);
+    
+    println!(" your data is ; {} this is your return data : {}",referencing_data ,return_data);
+
+
+
 }
 
 
+
+//  this is void function 
 fn _first_void_function(values : String){
 
    println!( "your data is  ---  {}" ,values);
 
 }
 
-
+//  this is non void function
 fn _first_function (number :i32) ->i32{
     let value :i32 = 10 ;
     println!("under the function {}",value);
@@ -36,3 +61,35 @@ fn _first_function (number :i32) ->i32{
     value2
 
 }
+
+
+
+// chack ownerShip
+
+fn _chack_owner_ship (data :String)-> String{
+    let data = data;
+    let data2 = data;
+
+    
+    println!(" owner function data : {}",data2 ); // thay give me problem for owner ship
+    data2
+}
+
+
+// referencing  
+
+fn _reference (    data  :&mut i32)->i32{
+    *data =*data - *data + 23;
+    let return_data = *data +20;
+
+
+    let chack_test= 12232;
+    let test_2 = &chack_test;
+    println!("your test data is : {} second test {}",&chack_test ,*test_2);
+
+
+    return_data
+    
+}
+
+
