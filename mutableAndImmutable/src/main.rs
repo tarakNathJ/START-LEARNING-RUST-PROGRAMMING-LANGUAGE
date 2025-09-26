e4c2bc1489886fe5199b90a3dfb49db6367cb5ae
Tarak Nath Jana <janatarak739@gmail.com>
2025-09-26 18:07:20 +0530

    save data

---
+++ mutableAndImmutable/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let mut   _num_ber:i32 = 120;
+
+//     _num_ber = 234;
+
+//     {
+//         let _num_ber = _num_ber+ 2;
+//         println!("Hello, world iner!{} : ",_num_ber);
+//     }
+
+//    println!("Hello, world outer!{} : ",_num_ber);
+
+ const  DATA :i32  = 10 *10;
+
+ _num_ber =  DATA + _num_ber ;
+ println!("{}",_num_ber);
+
+
+}
